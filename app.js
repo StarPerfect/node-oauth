c8f000d97aa56f2392b305b8c8a38e31349eda8d
StarPerfect <StarPerfect@gmail.com>
2020-04-18 18:11:07 -0600

    Add oktaClient adn ExpressOIDC middleware

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dashboardRouter = require("./routes/dashboard");
+const publicRouter = require("./routes/public");
 
 var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: '{yourOktaOrgUrl}',
+  token: '{yourOktaToken}'
+});
+const oidc = new ExpressOIDC({
+  issuer: "{yourOktaOrgUrl}/oauth2/default",
+  client_id: {
+    yourClientId
+  },
+  client_secret: {
+    yourClientSecret
+  },
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,17 +41,18 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'alkfjaoirjknvkfjvajgieojg',
+  secret: 'aeiklfjaefjaoej',
   resave: true,
   saveUninitialized: false
 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
